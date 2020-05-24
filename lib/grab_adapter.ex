e2dc5c9043ba813c973109f8e0fed2f90982532a
Michael Hanna <mykelkim@yahoo.com>
2020-05-24 18:57:25 -0400

    store values to map

--- lib/grab_adapter.ex
+++ lib/grab_adapter.ex
@@ -17,7 +17,7 @@ defmodule GrabAdapter do
     }
 
     :ets.new(:chunk_lookup, [:set, :protected, :named_table])
-    :ets.insert(:chunk_lookup, {"chunk", ""}) # start off with empty string
+    :ets.insert(:chunk_lookup, {ytchannel, ""}) # start off with empty string
 
 
     # :ets.lookup_element(:chunk_lookup, "chunk", 2)
@@ -29,6 +29,12 @@ defmodule GrabAdapter do
     # "python metadatagrabber.py ytchanneldata https://www.youtube.com/channel/UCMCMaXiCRuervjaVe7_gf8w"
     # da lata "https://www.youtube.com/channel/UCIlFrdZCFUPlvlbgdyu1xdQ"
 
+    # GrabAdapter.start_link("https://www.youtube.com/channel/UCMCMaXiCRuervjaVe7_gf8w")
+    # GrabAdapter.start_link("https://www.youtube.com/channel/UCIlFrdZCFUPlvlbgdyu1xdQ")
+    # GrabAdapter.start_link("https://www.youtube.com/channel/UCMCMaXiCRuervjaVe7_gf8w")
+
+    # GrabAdapter.start_link("https://www.youtube.com/channel/UC47eUBNO8KBH_V8AfowOWOw")
+
     cmd = "python metadatagrabber.py ytchanneldata #{ytchannel}"
     port = Port.open({:spawn, cmd}, [:binary, :exit_status])
     state = Map.put(state, :port, port)
@@ -39,7 +45,9 @@ defmodule GrabAdapter do
   def handle_info({port, {:exit_status, exit_status}}, state) do
     IO.puts "Received exit_status from port: #{exit_status}"
 
-    :ets.delete(:chunk_lookup, "chunk")
+    ytchannel = Map.get(state, :ytchannel)
+
+    :ets.delete(:chunk_lookup, ytchannel)
     :ets.delete(:chunk_lookup)
 
     {:noreply, state}
@@ -50,8 +58,10 @@ defmodule GrabAdapter do
 
     # {:ok, video} = Jaxon.decode(~s(#{msg}))
 
+    ytchannel = Map.get(state, :ytchannel)
+
     # fragment = @partial
-    fragment = :ets.lookup_element(:chunk_lookup, "chunk", 2)
+    fragment = :ets.lookup_element(:chunk_lookup, ytchannel, 2)
     # fragment = Map.get(state, "chunk")
 
 
@@ -66,18 +76,107 @@ defmodule GrabAdapter do
 
     intermediate = "#{fragment}#{msg}" # Enum.concat(fragment, msg) #[fragment | msg]
 
-    IO.puts "intermediate: #{intermediate}"
+    # IO.puts "intermediate: #{intermediate}"
+
+    video = %{}
 
     # intermediate = building_string #Enum.join(building_string,"")
     case Jason.decode(intermediate) do
       {:ok, _} ->
         stream = [~s(#{intermediate})]
-        title =
-          stream |> Jaxon.Stream.from_enumerable() |> Jaxon.Stream.query([:root, "title"]) |> Enum.to_list()
-          IO.puts "title: #{title}"
+        title = stream |> Jaxon.Stream.from_enumerable() |> Jaxon.Stream.query([:root, "title"]) |> Enum.to_list() |> List.flatten() |> List.first()
+        video = Map.put(video, "title", title)
+        # IO.puts "title:"
+        # IO.inspect(title)
+
+        channel_url = stream |> Jaxon.Stream.from_enumerable() |> Jaxon.Stream.query([:root, "channel_url"]) |> Enum.to_list() |> List.flatten() |> List.first()
+        video = Map.put(video, "channel", channel_url)
+        # IO.puts "channel_url:"
+        # IO.inspect(channel_url)
+
+        webpage_url = stream |> Jaxon.Stream.from_enumerable() |> Jaxon.Stream.query([:root, "webpage_url"]) |> Enum.to_list() |> List.flatten() |> List.first()
+        video = Map.put(video, "webpage_url", webpage_url)
+        # IO.puts "webpage_url:"
+        # IO.inspect(webpage_url)
+
+        uploader = stream |> Jaxon.Stream.from_enumerable() |> Jaxon.Stream.query([:root, "uploader"]) |> Enum.to_list() |> List.flatten() |> List.first()
+        video = Map.put(video, "uploader", uploader)
+        # IO.puts "uploader:"
+        # IO.inspect(uploader)
+
+        upload_date = stream |> Jaxon.Stream.from_enumerable() |> Jaxon.Stream.query([:root, "upload_date"]) |> Enum.to_list() |> List.flatten() |> List.first()
+
+        year = String.slice(upload_date, 0..3)
+        month = String.slice(upload_date, 4..5)
+        day = String.slice(upload_date, 6..7)
+
+        # NaiveDateTime.from_iso8601("2015-01-23 23:50:07")
+
+        {:ok, datetime} = NaiveDateTime.from_iso8601("#{year}-#{month}-#{day} 00:00:00")
+        video = Map.put(video, "upload_date", datetime)
+
+        # IO.puts "upload_date:"
+        # IO.inspect(upload_date)
+
+        uploader_url = stream |> Jaxon.Stream.from_enumerable() |> Jaxon.Stream.query([:root, "uploader_url"]) |> Enum.to_list() |> List.flatten() |> List.first()
+        video = Map.put(video, "uploader_url", uploader_url)
+        # IO.puts "uploader_url:"
+        # IO.inspect(uploader_url)
+
+        thumbnails = stream |> Jaxon.Stream.from_enumerable() |> Jaxon.Stream.query([:root, "thumbnails", :all]) |> Enum.to_list() |> List.flatten() |> List.first()
+        video = Map.put(video, "thumbnail_url", Map.get(thumbnails, "url"))
+        # IO.puts "thumbnails:"
+        # IO.inspect(Map.get(thumbnails, "url"))
+
+        description = stream |> Jaxon.Stream.from_enumerable() |> Jaxon.Stream.query([:root, "description"]) |> Enum.to_list() |> List.flatten() |> List.first()
+        video = Map.put(video, "description", description)
+        # IO.puts "description:"
+        # IO.inspect(description)
+
+        categories = stream |> Jaxon.Stream.from_enumerable() |> Jaxon.Stream.query([:root, "categories"]) |> Enum.to_list() |> List.flatten()
+        video = Map.put(video, "categories", categories)
+        # IO.puts "categories:"
+        # IO.inspect(categories)
+
+        license = stream |> Jaxon.Stream.from_enumerable() |> Jaxon.Stream.query([:root, "license"]) |> Enum.to_list() |> List.flatten() |> List.first()
+        video = Map.put(video, "license", license)
+        # IO.puts "license:"
+        # IO.inspect(license)
+
+        duration = stream |> Jaxon.Stream.from_enumerable() |> Jaxon.Stream.query([:root, "duration"]) |> Enum.to_list() |> List.flatten() |> List.first()
+        video = Map.put(video, "duration", duration)
+        # IO.puts "duration:"
+        # IO.inspect(duration)
+
+        tags = stream |> Jaxon.Stream.from_enumerable() |> Jaxon.Stream.query([:root, "tags"]) |> Enum.to_list() |> List.flatten()
+        video = Map.put(video, "tags", tags)
+        # IO.puts "tags:"
+        # IO.inspect(tags)
+
+        id = stream |> Jaxon.Stream.from_enumerable() |> Jaxon.Stream.query([:root, "id"]) |> Enum.to_list() |> List.flatten() |> List.first()
+        video = Map.put(video, "id", id)
+        # IO.puts "id:"
+        # IO.inspect(id)
+
+        view_count = stream |> Jaxon.Stream.from_enumerable() |> Jaxon.Stream.query([:root, "view_count"]) |> Enum.to_list() |> List.flatten() |> List.first()
+        video = Map.put(video, "view_count", view_count)
+        # IO.puts "view_count:"
+        # IO.inspect(view_count)
+
+        like_count = stream |> Jaxon.Stream.from_enumerable() |> Jaxon.Stream.query([:root, "like_count"]) |> Enum.to_list() |> List.flatten() |> List.first()
+        video = Map.put(video, "like_count", like_count)
+        # IO.puts "like_count:"
+        # IO.inspect(like_count)
+
+        dislike_count = stream |> Jaxon.Stream.from_enumerable() |> Jaxon.Stream.query([:root, "dislike_count"]) |> Enum.to_list() |> List.flatten() |> List.first()
+        video = Map.put(video, "dislike_count", dislike_count)
+        # IO.puts "dislike_count:"
+        # IO.inspect(dislike_count)
+
+        IO.inspect(video)
 
         # @partial = ""
-        :ets.insert(:chunk_lookup, {"chunk", ""})
+        :ets.insert(:chunk_lookup, {ytchannel, ""})
         # state = Map.put(state, "chunk", "")
 
 
@@ -85,7 +184,7 @@ defmodule GrabAdapter do
         IO.puts "there was an error decoding intermediate"
 
         # @partial = building_string
-        :ets.insert(:chunk_lookup, {"chunk", intermediate})
+        :ets.insert(:chunk_lookup, {ytchannel, intermediate})
         # state = Map.put(state, "chunk", intermediate)
 
     end
